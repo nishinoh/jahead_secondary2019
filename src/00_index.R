55388b6ac20974cc391429630d02d08c43d23606
nishinoh_MBP <ps026061@ed.ritsumei.ac.jp>
2019-08-19 20:29:21 +0900

    [add] ファイルを開かずに実行できるよう目次を作成

---
+++ src/00_index.R
@@ -0,0 +1,8 @@
+##### コードの目次 =============================================
+
+##### 0. Waveをマージする =====================================
+source("src/01_read_data_files.R", echo=TRUE)
+
+##### 1. 変数を新たに作成 ===================================
+source("src/11_generate_variables_1.R", echo=TRUE)
+source("src/12_generate_variables_2.R", echo=TRUE)
