cf8c4db74f28188e6ac8d0683ee152f8f79b54d0
nishinoh_DU <hnishino@tonichi-kokusai-u.ac.jp>
2019-10-10 20:11:36 +0900

    [update] 結果の図をいくつか追加

--- src/71d_run_mcmc_adl.R
+++ src/71d_run_mcmc_adl.R
@@ -6,4 +6,13 @@ fit_model <- stan(file="src/71c_hierarchical_model_adl_2.stan", data=list_stan_a
                   iter = 5000, warmup = 2500, thin=5)
 save(fit_model, file="~/Data/JAHEAD/Process_Files/stan_result_varying_intercept_adl1.rda")
 fit_model
-stan_trace(fit_model)
+# トレースプロット
+stan_trace(fit_model, inc_warmup = TRUE)
+# 事後分布
+stan_dens(fit_model, separate_chains = TRUE)
+# 自己相関
+stan_ac(fit_model)
+# R_hatの分布
+stan_rhat(fit_model)
+# 実効サンプルサイズ
+stan_ess(fit_model)
