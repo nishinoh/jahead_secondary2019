bd2a9a42deb189c4dbd303179cb42af11ffc11ef
nishinoh_DU <hnishino@tonichi-kokusai-u.ac.jp>
2019-10-10 19:18:15 +0900

    [fix] エラー修正

--- src/72d_run_mcmc_iadl.R
+++ src/72d_run_mcmc_iadl.R
@@ -2,8 +2,10 @@ library(rstan)
 library(ggmcmc)
 
 load("~/Data/JAHEAD/Process_Files/data_after_72.rda")
-fit_model_iadl <- stan(file="src/72b_hierarchical_model_adl_2.stan", data=list_stan_adl, cores = 4, seed=1234,
-                  iter = 2000, warmup = 1000, thin=5)
+fit_model_iadl <- stan(file="src/72b_hierarchical_model_iadl_1.stan", data=list_stan_adl, cores = 4, seed=1234,
+                  iter = 5000, warmup = 2500, thin=5)
 save(fit_model, file="~/Data/JAHEAD/Process_Files/stan_result_varying_intercept_iadl1.rda")
-fit_model
+
+# 結果の表示
+fit_model_iadl
 stan_trace(fit_model_iadl)
