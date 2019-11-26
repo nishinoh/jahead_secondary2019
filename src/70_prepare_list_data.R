6c952b3c86295fbd987c792052ca5e6347d56afb
nishinoh_iM27 <ps026061@ed.ritsumei.ac.jp>
2019-11-27 08:37:05 +0900

    [change] Stanに渡すデータを、DFを作るファイルを先に独立させる(リスト化は別ファイルで)

--- src/70_prepare_list_data.R
+++ src/70_prepare_list_data.R
@@ -1,6 +1,8 @@
 ##### このファイルについて ========================================================
-# Stanに渡すデータを作成する
-# 両方とも共通するケースを抜き出して、必要であればリストの段階でADLとIADLのデータを分ける
+# Stanに渡すデータを特定する
+# 利用するケースを抜き出してデータフレームを作成。
+# ここで欠損が全くないデータフレームを作っておく。
+# Stanに渡すリスト形式のデータは、ここで作成したデータフレームから削り出して使う。
 
 ##### 0. 前準備 ========================================================
 # library(tidyverse)
@@ -14,11 +16,13 @@ library(readr)
 load("~/Data/JAHEAD/Process_Files/data_after_14.rda")
 load("~/Data/JAHEAD/Process_Files/data_after_23.rda")
 
-##### 1. 子どものデータを抜き出してリスト化 ======================================================
-# リストにしてStanに渡すべきケースをここで完全に特定する
+##### 1. Stanに渡せる、欠損のないケースをここで完全に特定する ======================================================
+# ここで作成したデータから必要なものを、モデルごとに切り出してリスト化する
+# リストにするのは別のファイルで行う。
 
 data_complete_cases <- data_united %>% 
     # Stanに投入する変数だけ抜き出す
+    # ここで抽出したケースが分析のサンプルサイズを決めるので、モデルで使わない変数は落とす
     select(id_personyear_child_d, id_personyear_child, id_personyear, id_text,
            do_care_parents_adl, #ADLで支援のある人を抜き出す
            do_care_parents_iadl, #IADLで支援のある人を抜き出す
@@ -35,49 +39,28 @@ data_complete_cases <- data_united %>%
                                   ch_married == "いいえ" ~ 0),
            is_real_child = case_when(is_child == "子ども" ~ 1,
                                      is_child == "子の配偶者" ~ 0)) %>% 
+    mutate(t_female = case_when(t_gender == "女性" ~ 1,
+                                t_gender == "男性" ~ 0)) %>% 
     # ここでStanに渡すべきデータが完全に特定
     filter(complete.cases(.)) %>% 
     # Stanで使う共通IDを作成。各種のIDを1からの連番にする。
     arrange(id_personyear_child_d) %>% 
     mutate(id_personyear_child_d_n = as.factor(id_personyear_child_d),
            id_personyear_child_n = as.factor(id_personyear_child),
-           id_personyear_n = as.factor(id_personyear)) %>% 
+           id_personyear_n = as.factor(id_personyear),
+           id_n = as.factor(id_text)) %>% 
     mutate(id_personyear_child_d_n = unclass(id_personyear_child_d_n),
            id_personyear_child_n = unclass(id_personyear_child_n),
-           id_personyear_n = unclass(id_personyear_n))
+           id_personyear_n = unclass(id_personyear_n),
+           id_n = unclass(id_n))
 
-##### ダイアドで使う変数だけ抜き出す ====================================================
-data_stan_child <- data_complete_cases %>% 
-    select(id_personyear_child_d_n, id_personyear_child_n, id_personyear_n,
-           do_care_parents_adl, do_care_parents_iadl, 
-           is_real_child, ch_female, ch_married, ch_dist_living_l)
-
-##### 回答者の各時点データを抜き出す =====================================================
-data_stan_panel <- data_complete_cases %>% 
-    # dplyr::distinct()が、重複している行を省いてくれる関数
-    distinct(id_personyear, .keep_all = TRUE) %>% 
-    mutate(t_female = case_when(t_gender == "女性" ~ 1,
-                                t_gender == "男性" ~ 0)) %>% 
-    arrange(id_personyear_n) %>% 
-    select(id_personyear, id_personyear_n, t_age, t_female, lim_adl, lim_iadl,
-           use_dayservice_n, use_homehelp_n,
-           living_spouse)
-    # mutate(use_dayservice_n = if_else(use_dayservice_n == 0, 0, 1))
-
-# 念のためIDが問題なく作れているかチェック
-# max(data_stan_child$id_personyear_child_n)
-# max(data_stan_panel$id_personyear_n)
+# 念のためIDが問題なく作れているかチェック(データ名変わっているので使うとき修正)
+# max(data_complete_cases$id_personyear_child_n)
+# max(data_complete_cases$id_personyear_n)
 # cor.test(1:nrow(data_stan_child), data_stan_child$id_personyear_child_n)
 # cor.test(1:nrow(data_stan_panel), data_stan_panel$id_personyear_n)
-
-##### リストにする ===================================
-list_stan_adl <- c(list(C = nrow(data_stan_child)),
-                   list(P = nrow(data_stan_panel)),
-                   as.list(data_stan_child),
-                   as.list(data_stan_panel))
 
 ##### Fin. 作成したファイルを保存 ================================================
 # 作成したファイルを保存し、これまで作ったオブジェクトはいったん全て削除
 save(data_complete_cases, file = "~/Data/JAHEAD/Process_Files/data_after_70_data_frame.rda")
-save(list_stan_adl, file="~/Data/JAHEAD/Process_Files/data_after_70.rda")
 rm(list = ls())
