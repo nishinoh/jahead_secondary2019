0c95846bf7bc3e3cf634db8992de386d297ba199
nishinoh_iM27 <ps026061@ed.ritsumei.ac.jp>
2019-11-27 23:25:37 +0900

    [update] 3レベルのモデルを作成

---
+++ src/75a_76a_prepare_list_for_3level_models.R
@@ -0,0 +1,49 @@
+##### このファイルについて ========================================================
+# Stanに渡すデータを作成する
+# 両方とも共通するケースを抜き出して、必要であればリストの段階でADLとIADLのデータを分ける
+
+##### このファイルについて ========================================================
+# Stanに渡すデータを作成する
+# 両方とも共通するケースを抜き出して、必要であればリストの段階でADLとIADLのデータを分ける
+
+##### 0. 前準備 ========================================================
+# library(tidyverse)
+library(dplyr)
+
+load("~/Data/JAHEAD/Process_Files/data_after_70_data_frame.rda")
+
+##### 1. 完全な個人単位(I)の変数だけ抜き出す ====================================================
+data_stan_individuals <- data_complete_cases %>% 
+    # IDは自分の階層と1つ上の階層のものだけ含めないとエラーになる
+    select(id_personyear_child_d_n, id_personyear_child_n,
+           do_care_parents_adl, do_care_parents_iadl, 
+           is_real_child, ch_female, ch_married)
+
+##### 2. 子ども夫婦単位(C)の変数だけ抜き出す =====================================================
+data_stan_childs_couples <- data_complete_cases %>% 
+    distinct(id_personyear_child, .keep_all = TRUE) %>% 
+    select(# id_personyear_child,id_personyear, id_text,
+           id_personyear_child_n, id_personyear_n,
+           ch_dist_living_l)
+
+##### 3. 回答者のパーソンイヤー単位(P)の変数だけ抜き出す =====================================================
+data_stan_panels <- data_complete_cases %>% 
+    # dplyr::distinct()が、重複している行を省いてくれる関数
+    distinct(id_personyear, .keep_all = TRUE) %>% 
+    arrange(id_personyear_n) %>% 
+    select(id_personyear_n, id_n, t_age, lim_adl, lim_iadl,
+           use_dayservice_n, use_homehelp_n,
+           living_spouse, t_female)
+
+##### 4レベルのモデルに渡すリストを作成 ===================================
+list_stan_3levels <- c(list(I = nrow(data_stan_individuals)),
+                       list(C = nrow(data_stan_childs_couples)),
+                       list(P = nrow(data_stan_panels)),
+                       as.list(data_stan_individuals),
+                       as.list(data_stan_childs_couples),
+                       as.list(data_stan_panels))
+
+##### Fin. 作成したファイルを保存 ================================================
+# 作成したファイルを保存し、これまで作ったオブジェクトはいったん全て削除
+save(list_stan_3levels, file="~/Data/JAHEAD/Process_Files/data_after_75a_76a.rda")
+rm(list = ls())
