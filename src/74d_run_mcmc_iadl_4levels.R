396ce47754460131f5a0caf474da21a660f063b6
nishinoh_DU <hnishino@tonichi-kokusai-u.ac.jp>
2019-11-28 14:28:55 +0900

    [update] IADLについて4レベルのモデルで推定

---
+++ src/74d_run_mcmc_iadl_4levels.R
@@ -0,0 +1,19 @@
+library(rstan)
+library(ggmcmc)
+
+load("~/Data/JAHEAD/Process_Files/data_after_73a_74a.rda")
+fit_model <- stan(file="src/74b_hierarchical_model_iadl_4levels.stan", data=list_stan_4levels, cores = 4, seed=1234,
+                  iter = 25000, warmup = 5000, thin = 5)
+save(fit_model, file="~/Data/JAHEAD/Process_Files/stan_result_varying_intercept_adl_4levels.rda")
+fit_model
+# トレースプロット
+stan_trace(fit_model, inc_warmup = TRUE)
+stan_trace(fit_model)
+# 事後分布
+stan_dens(fit_model, separate_chains = TRUE)
+# 自己相関
+stan_ac(fit_model)
+# R_hatの分布
+stan_rhat(fit_model)
+# 実効サンプルサイズ
+stan_ess(fit_model)
