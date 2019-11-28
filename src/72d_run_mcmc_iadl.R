7c06f3a03a2d14fdd9d84e64d7f6ba32e1560232
nishinoh_DU <hnishino@tonichi-kokusai-u.ac.jp>
2019-11-28 14:27:57 +0900

    [update] IADLについての4レベルのモデルの設定を更新

--- src/72d_run_mcmc_iadl.R
+++ src/72d_run_mcmc_iadl.R
@@ -3,8 +3,8 @@ library(ggmcmc)
 
 load("~/Data/JAHEAD/Process_Files/data_after_70.rda")
 fit_model_iadl <- stan(file="src/72b_hierarchical_model_iadl_1.stan", data=list_stan_adl, cores = 4, seed=1234,
-                  iter = 10000, warmup = 5000, thin=5)
-save(fit_model, file="~/Data/JAHEAD/Process_Files/stan_result_varying_intercept_iadl1.rda")
+                  iter = 25000, warmup = 5000, thin=5)
+save(fit_model_iadl, file="~/Data/JAHEAD/Process_Files/stan_result_varying_intercept_iadl_2levels.rda")
 
 # 結果の表示
 fit_model_iadl
