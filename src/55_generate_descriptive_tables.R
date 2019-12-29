076c3cc17a65d55afdc51d8fa214d7dc7c43a4e8
nishinoh_MBP <ps026061@ed.ritsumei.ac.jp>
2019-12-29 19:32:22 +0900

    [update] ADLとIADL別に、ケアを受けている人の数を集計

--- src/55_generate_descriptive_tables.R
+++ src/55_generate_descriptive_tables.R
@@ -1,10 +1,10 @@
 ##### 0. 前準備 ========================================================
 library(tidyverse)
-library(plm)
 library(stargazer)
 library(xtable)
 
 load("~/Data/JAHEAD/Process_Files/data_after_41_data_frame.rda")
+load("~/Data/JAHEAD/Process_Files/data_after_14.rda") #ADLケアとIADLケアの被提供をみるため
 
 dir <- "~/Dropbox/00_Projects/dropbox_jahead_secondary2019/tab/"
 
@@ -55,12 +55,24 @@ discriptive_table <- tab_i_level %>%
     bind_rows(tab_c_level, tab_p_level, tab_r_level) %>% 
     xtable(., label="tab_discriptive_table", caption = "記述統計量", digits = c(0,0,0,2,2,0,0))
 
-# xtable(out, label="tab_data_qca", caption="今回のQCAで用いるデータ", digits =c(0,0,0,1,2,2,2,0,2,0,2,2,2))
-
-print(discriptive_table, file=str_c(dir, "test.tex"),
+print(discriptive_table, file=str_c(dir, "descriptive_statistics.tex"),
       caption.placement = "top", table.placement = "htbp", include.rownames = FALSE)
 
+# as.data.frame(tmp) %>% 
+    # stargazer(., out=str_c(dir, "test.tex"), title = "Iレベルの記述統計", style="ajs", omit.summary.stat = c("p75", "p25"))
 
+#####  ======================================
+data_complete_cases %>% 
+    distinct(id_personyear, .keep_all=TRUE) %>% 
+    names()
 
-as.data.frame(tmp) %>% 
-    stargazer(., out=str_c(dir, "test.tex"), title = "Iレベルの記述統計", style="ajs", omit.summary.stat = c("p75", "p25"))
+list <- data_complete_cases %>% 
+    distinct(id_personyear)
+
+# ADLのケアを受けている人と、IADLのケアを受けている人で4つに分けて人数を出す
+data_long %>% 
+    filter(id_personyear %in% list$id_personyear) %>% 
+    mutate(exist_helper_adl_d = if_else(exist_helper_adl_l > 2, "ADLの介護者あり", "ADLの介護者なし"),
+           exist_helper_iadl_d = if_else(exist_helper_iadl_l > 2, "IADLの介護者あり", "IADLの介護者なし")) %>% 
+    count(exist_helper_adl_d, exist_helper_iadl_d)
+# なぜか両方の質問でNAの人がいる。遡って検証
